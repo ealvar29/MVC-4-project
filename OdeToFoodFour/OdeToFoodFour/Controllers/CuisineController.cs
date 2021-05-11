e4879c807ec446cdbd02a04e5eeb8222c6880eb7
Eduardo <ealvarez@carminaticonsulting.com>
2021-05-10 20:21:34 -0500

    Lesson 2

---
+++ OdeToFoodFour/OdeToFoodFour/Controllers/CuisineController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFoodFour.Controllers
+{
+    public class CuisineController : Controller
+    {
+        [Authorize]
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+            return Content("Hello World" + message);
+        }
+    }
+}
---
+++ OdeToFoodFour/OdeToFoodFour/Controllers/ReviewsController.cs
@@ -0,0 +1,126 @@
+﻿using OdeToFoodFour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFoodFour.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult BestReview()
+        {
+            var bestReview = _reviews.OrderByDescending(x => x.Rating);
+            return PartialView("_Review", bestReview.First());
+        }
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var model = _reviews.OrderBy(x => x.Country);
+            return View(model);
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var review = _reviews.Single(x => x.Id == id);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var review = _reviews.Single(x => x.Id == id);
+
+            if (TryUpdateModel(review))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Cinnamon Club",
+                City = "London",
+                Country = "UK",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 2,
+                Name = "Marrakesh",
+                City = "D.C.",
+                Country = "USA",
+                Rating = 8
+            },
+            new RestaurantReview
+            {
+                Id = 3,
+                Name = "The House of Elliot",
+                City = "Ghent",
+                Country = "Belgium",
+                Rating = 8
+            },
+        };
+
+    }
+}
